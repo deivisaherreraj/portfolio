ff4c84c951a9a8f90664385a4e23cfc6f382013d
Deivis Herrera <deivisaherreraj@gmail.com>
2025-10-29 18:12:23 -0500

    Elimina el enlace de 'Home' de la lista de navegación y añade un enlace traducido para 'Home' en el componente de encabezado.

--- src/app/app-core/data/nav-links.ts
+++ src/app/app-core/data/nav-links.ts
@@ -1,7 +1,6 @@
 import { NavLinks } from "@applayout/header/models/nav-links.interface";
 
 export const navLinks: NavLinks[] = [
-    { key: 'Home', value: 'home' },
     { key: 'About', value: 'about' },
     { key: 'Projects', value: 'projects' },
     { key: 'Experience', value: 'experience' },
